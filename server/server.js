bdefc4233f6c69171da6f43b43b293057ae923a7
Голосняк Юлия Викторовна <golosnyak.yv1984@okeit.edu>
2026-03-19 16:51:53 +0500

    sdfsdfsfs

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.use('/api/visits', visitRoutes);
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-app.use(express.static('dist'));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
